570137cf4cc6a47a843809a72b8ada4d579b0625
kebele <ykemalbeler@gmail.com>
2021-01-09 17:26:12 +0300

    update

--- react_anime_search/src/App.js
+++ react_anime_search/src/App.js
@@ -1,10 +1,22 @@
 // import logo from './logo.svg';
 // import './App.css';
+import { useState, useEffect } from 'react'
+import Header from './components/Header'
+
 
 function App() {
+
+  const [ animeList, setAnimeList ]  = useState([])
+  const [ topAnime, setTopAnime ] = useState([])
+  const [ search, setSearch ] = useState("")
+
+
   return (
     <div className="App">
-    selam
+      <Header />
+      <div className="content-wrap">
+        
+      </div>
     </div>
   );
 }
