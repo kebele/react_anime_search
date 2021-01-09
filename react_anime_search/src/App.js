66f203e0ed5400f308b20e9ac3f931884a3092be
kebele <ykemalbeler@gmail.com>
2021-01-10 02:41:28 +0300

    update

--- react_anime_search/src/App.js
+++ react_anime_search/src/App.js
@@ -2,6 +2,8 @@
 // import './App.css';
 import { useState, useEffect } from 'react'
 import Header from './components/Header'
+import Sidebar from './components/Sidebar'
+import MainContent from './components/MainContent'
 
 
 function App() {
@@ -10,12 +12,57 @@ function App() {
   const [ topAnime, setTopAnime ] = useState([])
   const [ search, setSearch ] = useState("")
 
+//TOP 10 anime listesi için
+  const GetTopAnime = async () => {
+    const temp = await fetch ('https://api.jikan.moe/v3/top/anime/1/bypopularity')
+      .then(res => res.json())
+
+      setTopAnime(temp.top.slice(0,10));
+  }
+
+  //SEARCH sonuçları için 
+  const HandleSearch = e => {
+    e.preventDefault();
+    
+    console.log(search);
+    FetchAnime(search);
+  }
+
+  const FetchAnime = async (query) => {
+    const temp = await fetch(`https://api.jikan.moe/v3/search/anime?q=${query}&order_by=title&sort=asc&limit=10`)
+
+    .then(res => res.json());
+
+      
+
+console.log(temp.results);
+
+      setAnimeList(temp.results)
+  }
+
+
+
+  useEffect(() => {
+    GetTopAnime()
+
+    // console.log("topAnime");
+  },[])
+
+  // console.log(topAnime);
+
 
   return (
-    <div className="App">
+    <div className = "App">
       <Header />
-      <div className="content-wrap">
-        
+      <div className ="content-wrap">
+        <Sidebar 
+          topAnime = { topAnime }/>
+          <MainContent 
+              HandleSearch = { HandleSearch }
+              search = { search }
+              setSearch = { setSearch }
+              animeList = { animeList}
+          />
       </div>
     </div>
   );
---
+++ react_anime_search/src/components/Sidebar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function Sidebar({ topAnime }) {
+    return (
+        <aside>
+            <nav>
+                <h3>top anime</h3>
+                {topAnime.map(anime => (
+                    <a 
+                    href={ anime.url} 
+                    target="_blank" 
+                    rel="noreferrer"
+                    key={ anime.mal_id}>
+                    { anime.title }
+                </a>
+                ))}
+                
+              
+            </nav>
+        </aside>
+    )
+}
+
+export default Sidebar
